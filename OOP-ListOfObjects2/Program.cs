ceb02495c741061895540bfd369cfbefc63c76e8
janthony25 <junanthony.business@gmail.com>
2024-02-04 21:23:48 +1300

    Add project files.

---
+++ OOP-ListOfObjects2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<Students> students = new List<Students>();
+
+         
+
+            students.Add(new Students("Jun"));
+            students.Add(new Students("Jb"));
+            students.Add(new Students("Josh"));
+
+            foreach (Students student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Students
+    {
+        string name;
+
+        public Students(string name)
+        {
+            this.name = name;
+        }
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
